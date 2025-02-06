8eefa35d42686540fae60a8c0fe8471b05c5dafc
Rico Cassoni <rcassoni@sdccd.edu>
2025-02-06 11:51:08 -0800

    Completed all lines for diamon in ProChal4

--- ProChal4.java
+++ ProChal4.java
@@ -10,5 +10,10 @@ public class ProChal4
     public static void main (String[] args){
         System.out.println("   *");
         System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
     }
 }
